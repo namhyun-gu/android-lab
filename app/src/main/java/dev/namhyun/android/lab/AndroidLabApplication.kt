a8fa47b532f9f7f41fa73b7749e3bdb035e156b0
namhyun-gu <namhyun.gu@webtoonscorp.com>
2022-03-20 22:35:34 +0900

    Add dagger hilt

---
+++ app/src/main/java/dev/namhyun/android/lab/AndroidLabApplication.kt
@@ -0,0 +1,7 @@
+package dev.namhyun.android.lab
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AndroidLabApplication : Application()
--- app/src/main/java/dev/namhyun/android/lab/ui/MainActivity.kt
+++ app/src/main/java/dev/namhyun/android/lab/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package dev.namhyun.android.lab
+package dev.namhyun.android.lab.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.namhyun.android.lab.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
